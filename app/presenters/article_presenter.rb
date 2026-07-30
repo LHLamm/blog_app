626b7145968532e159b4c285741c2b69d034de19
LHLamm <lhlam2003@gmail.com>
2026-07-30 15:43:05 +0700

    chore: bump rails to patch CVE-2026-66066 in activestorage

--- app/presenters/article_presenter.rb
+++ app/presenters/article_presenter.rb
@@ -1,7 +1,21 @@
 class ArticlePresenter < SimpleDelegator
+  def initialize(article, current_user:, view_context:)
+    super(article)
+    @current_user = current_user
+    @view_context = view_context
+  end
+
   def formatted_created_at
     I18n.l(created_at, format: :short)
   rescue I18n::ArgumentError, ArgumentError
     created_at.strftime("%b %d, %Y")
+  end
+
+  def author_display
+    if author == @current_user
+      @view_context.link_to(author.name, @view_context.user_path(author))
+    else
+      author.name
+    end
   end
 end
